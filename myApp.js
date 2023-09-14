bf5ed742fe96c647390cdd0cde5a26a119fb39f5
walwyde <walwyde@gmail.com>
2023-09-14 15:52:51 +0100

    file served

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ let app = express();
 // console.log("Hello World");
 
 app.get("/", (req, res) => {
-  res.send("Hello Express");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 module.exports = app;
