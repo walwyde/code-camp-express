c35fd634d73ddf5a055bf974569fe2b36f40e2b1
walwyde <walwyde@gmail.com>
2023-09-14 22:32:18 +0100

    public file served

--- myApp.js
+++ myApp.js
@@ -3,6 +3,8 @@ let app = express();
 
 // console.log("Hello World");
 
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
